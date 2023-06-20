376a4b10250edd9805a271041af881fc2ab15b5c
nown1ne <abhinav14863@gmail.com>
2023-06-20 19:30:59 +0530

    inorder-trav

---
+++ 94.binary-tree-inorder-traversal.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode* curr = root;
+
+        while(curr!=NULL){
+        if(curr->left==NULL){
+            ans.push_back(curr->val);
+            curr=curr->right;
+        }
+        else{
+            TreeNode* prev = curr->left;
+            while(prev->right && prev->right!=curr)
+                prev=prev->right;
+
+            if(prev->right==NULL){
+                prev->right=curr;
+                curr=curr->left;
+            }    
+            else{
+                prev->right=NULL;
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+        }
+        }
+        return ans;
+    }
+};
